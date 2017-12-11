4636834ae04b7d3f9513ffb878c135e956d6c38c
cjs07 <cjschaefer1999@gmail.com>
2017-12-11 17:55:58 -0500

    Xome new camera and rendering work, unimplemented

--- src/main/java/com/deepwelldevelopment/spacequest/Camera.java
+++ src/main/java/com/deepwelldevelopment/spacequest/Camera.java
@@ -5,9 +5,13 @@ import org.joml.Vector3f;
 
 import static java.lang.Math.asin;
 import static java.lang.Math.toDegrees;
-import static org.lwjgl.glfw.GLFW.glfwSetCursorPos;
+import static org.lwjgl.glfw.GLFW.*;
 
 public class Camera {
+
+    public static final float STEP_SCALE = 1.0f;
+    public static final float EDGE_STEP = 0.5f;
+    public static final int MARGIN = 10;
 
     long window;
     int windowWidth;
@@ -27,6 +31,20 @@ public class Camera {
 
     Vector2i mousePos;
 
+    public Camera(long window, int windowWidth, int windowHeight) {
+        this.window = window;
+        this.windowWidth = windowWidth;
+        this.windowHeight = windowHeight;
+        this.pos = new Vector3f(0.0f, 0.0f, 0.0f);
+        target = new Vector3f(0.0f, 0.0f, 0.0f);
+        target.normalize();
+        up = new Vector3f(0.0f, 0.0f, 0.0f);
+
+        mousePos = new Vector2i(0, 0);
+
+        init();
+    }
+
     public Camera(long window, int windowWidth, int windowHeight, Vector3f pos, Vector3f target, Vector3f up) {
         this.window = window;
         this.windowWidth = windowWidth;
@@ -38,6 +56,8 @@ public class Camera {
 
         this.up = up;
         up.normalize();
+
+        mousePos = new Vector2i(0, 0);
 
         init();
     }
@@ -68,7 +88,36 @@ public class Camera {
         onRightEdge = false;
         mousePos.x = windowWidth / 2;
         mousePos.y = windowHeight / 2;
-        glfwSetCursorPos(window, windowWidth / 2, windowHeight / 2);
+//        glfwSetCursorPos(window, windowWidth / 2, windowHeight / 2);
+    }
+
+    boolean onKeyboard(int key) {
+        boolean ret = false;
+        switch (key) {
+            case GLFW_KEY_W:
+                pos.add(target.mul(STEP_SCALE));
+                ret = true;
+                break;
+            case GLFW_KEY_S:
+                pos.sub(target.mul(STEP_SCALE));
+                ret = true;
+                break;
+            case GLFW_KEY_A:
+                Vector3f left = target.cross(up);
+                left.normalize();
+                left.mul(STEP_SCALE);
+                pos.add(left);
+                ret = true;
+                break;
+            case GLFW_KEY_D:
+                Vector3f right = target.cross(target);
+                right.normalize();
+                right.mul(STEP_SCALE);
+                pos.add(right);
+                ret = true;
+                break;
+        }
+        return ret;
     }
 
     void onMouse(int x, int y) {
@@ -80,15 +129,86 @@ public class Camera {
 
         hAngle += (float) deltaX / 20.0f;
         vAngle += (float) deltaY / 20.0f;
-    }
 
-    void update() {
+        if (deltaX == 0) {
+            if (x <= MARGIN) {
+//                hAngle -= 1.0f;
+                onLeftEdge = true;
+            } else if (x >= (windowWidth - MARGIN)) {
+//                hAngle += 1.0f;
+                onRightEdge = true;
+            }
+        } else {
+            onLeftEdge = false;
+            onRightEdge = false;
+        }
 
+        if (deltaY == 0) {
+            if (y <= MARGIN) {
+                onUpperEdge = true;
+            }
+            else if (y >= (windowHeight - MARGIN)) {
+                onLowerEdge = true;
+            }
+        }
+        else {
+            onUpperEdge = false;
+            onLowerEdge = false;
+        }
+
+        update();
     }
 
     void onRender() {
+        boolean shouldUpdate = true;
 
+        if (onLeftEdge) {
+            hAngle -= EDGE_STEP;
+            shouldUpdate = true;
+        }
+        else if (onRightEdge) {
+            hAngle+= EDGE_STEP;
+            shouldUpdate = true;
+        }
+
+        if (onUpperEdge) {
+            if (vAngle > -90.0f) {
+                vAngle -= EDGE_STEP;
+                shouldUpdate = true;
+            }
+        }
+        else if (onLowerEdge) {
+            if (vAngle < 90.0f) {
+                vAngle += EDGE_STEP;
+                shouldUpdate = true;
+            }
+        }
+
+        if (shouldUpdate) {
+            update();
+        }
     }
+
+    void update() {
+        Vector3f vAxis = new Vector3f(0.0f, 1.0f, 0.0f);
+
+        // Rotate the view vector by the horizontal angle around the vertical axis
+        Vector3f view = new Vector3f(1.0f, 0.0f, 0.0f);
+        view.rotateY(hAngle);
+        view.normalize();
+
+        // Rotate the view vector by the vertical angle around the horizontal axis
+        Vector3f hAxis = vAxis.cross(view);
+        hAxis.normalize();
+        view.rotateX(vAngle);
+
+        target = view;
+        target.normalize();
+
+        up = target.cross(hAxis);
+        up.normalize();
+    }
+
 
     public Vector3f getPos() {
         return pos;
