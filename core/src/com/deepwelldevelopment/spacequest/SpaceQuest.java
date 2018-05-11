6ff385a9a72c524b2b3f3965ff775ef610ec8729
cjs8487 <cjschaefer1999@gmail.com>
2018-05-10 23:55:11 -0400

    Basic textured cube

--- core/src/com/deepwelldevelopment/spacequest/SpaceQuest.java
+++ core/src/com/deepwelldevelopment/spacequest/SpaceQuest.java
@@ -1,33 +1,83 @@
 package com.deepwelldevelopment.spacequest;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.attributes.TextureAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.CameraInputController;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
-public class SpaceQuest extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+public class SpaceQuest implements ApplicationListener {
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+    private Environment environment;
+    private PerspectiveCamera cam;
+    private ModelBatch modelBatch;
+    private Model model;
+    private Texture texture;
+    private ModelInstance instance;
+    private CameraInputController camController;
+
+    @Override
+    public void create() {
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f));
+        modelBatch = new ModelBatch();
+        cam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        cam.position.set(10f, 10f, 10f);
+        cam.lookAt(0, 0, 0);
+        cam.near = 1f;
+        cam.far = 300f;
+        cam.update();
+
+        texture = new Texture("texture.png");
+        ModelBuilder builder = new ModelBuilder();
+//        builder.begin();
+//
+//        builder.end();
+        model = builder.createBox(5f, 5f, 5f,
+                new Material(TextureAttribute.createDiffuse(texture)),
+                Usage.Position | Usage.Normal | Usage.TextureCoordinates);
+        instance = new ModelInstance(model);
+
+        camController = new CameraInputController(cam);
+        Gdx.input.setInputProcessor(camController);
+    }
+
+    @Override
+    public void render() {
+        camController.update();
+
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
+
+        modelBatch.begin(cam);
+        modelBatch.render(instance, environment);
+        modelBatch.end();
+    }
+
+    @Override
+    public void dispose() {
+        texture.dispose();
+        modelBatch.dispose();
+        model.dispose();
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void resize(int width, int height) {
+    }
+
+    @Override
+    public void pause() {
+    }
 }
