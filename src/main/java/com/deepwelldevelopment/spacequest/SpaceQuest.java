4f3f1d2e9329ac4868ba6c7ac322edf00a4df67e
cjs8487 <cjschaefer1999@gmail.com>
2020-04-06 18:44:51 -0400

    Basic rendering

--- src/main/java/com/deepwelldevelopment/spacequest/SpaceQuest.java
+++ src/main/java/com/deepwelldevelopment/spacequest/SpaceQuest.java
@@ -1,5 +1,6 @@
 package com.deepwelldevelopment.spacequest;
 
+import org.lwjgl.BufferUtils;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.glfw.GLFWVidMode;
@@ -11,12 +12,26 @@ import java.nio.IntBuffer;
 import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
 import static org.lwjgl.glfw.GLFW.*;
 import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.opengl.GL30.glBindVertexArray;
+import static org.lwjgl.opengl.GL30.glGenVertexArrays;
 import static org.lwjgl.system.MemoryStack.stackPush;
 import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class SpaceQuest {
 
     private long window;
+
+    private String vertexShaderSource = "#version 330 core\n" +
+            "layout(location = 0) in vec3 aPos;\n" +
+            "void main() {\n" +
+            "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" +
+            "}";
+    private String fragmentShaderSource = "#version 330 core\n" +
+            "out vec4 FragColor;\n" +
+            "void main() {\n" +
+            "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n" +
+            "}";
 
     public void run() {
         System.out.println("Hello LWJGL " + Version.getVersion() + "!");
@@ -40,6 +55,8 @@ public class SpaceQuest {
         glfwDefaultWindowHints();
         glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 
         window = glfwCreateWindow(300, 300, "SpaceQuest", NULL, NULL);
         if (window == NULL) {
@@ -47,13 +64,13 @@ public class SpaceQuest {
         }
 
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-           if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
-               glfwSetWindowShouldClose(window, true);
-           }
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE) {
+                glfwSetWindowShouldClose(window, true);
+            }
         });
 
         try (MemoryStack stack = stackPush()) {
-            IntBuffer pWidth  = stack.mallocInt(1);
+            IntBuffer pWidth = stack.mallocInt(1);
             IntBuffer pHeight = stack.mallocInt(1);
 
             glfwGetWindowSize(window, pWidth, pHeight);
@@ -77,8 +94,67 @@ public class SpaceQuest {
 
         glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
 
+        float[] vertices = {
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f
+        };
+
+        IntBuffer intBuffer = BufferUtils.createIntBuffer(1);
+
+        glGenVertexArrays(intBuffer);
+        int vao = intBuffer.get(0);
+        glGenBuffers(intBuffer);
+        int vbo = intBuffer.get(0);
+
+        glBindVertexArray(vao);
+        glBindBuffer(GL_ARRAY_BUFFER, vbo);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+        int vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertexShader, vertexShaderSource);
+        glCompileShader(vertexShader);
+        glGetShaderiv(vertexShader, GL_COMPILE_STATUS, intBuffer);
+        if (intBuffer.get(0) == 0) {
+            String info = glGetShaderInfoLog(vertexShader);
+            System.err.println("Failed to compile vertex shader: " + info);
+            return;
+        }
+
+        int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragmentShader, fragmentShaderSource);
+        glCompileShader(fragmentShader);
+        glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, intBuffer);
+        if (intBuffer.get(0) == 0) {
+            String info = glGetShaderInfoLog(fragmentShader);
+            System.err.println("Failed to compile fragment shader: " + info);
+            return;
+        }
+
+        int shaderProgram = glCreateProgram();
+        glAttachShader(shaderProgram, vertexShader);
+        glAttachShader(shaderProgram, fragmentShader);
+        glLinkProgram(shaderProgram);
+        glGetProgramiv(shaderProgram, GL_LINK_STATUS, intBuffer);
+        if (intBuffer.get(0) == 0) {
+            String info = glGetProgramInfoLog(shaderProgram);
+            System.err.println("Failed to link program: " + info);
+            return;
+        }
+        glUseProgram(shaderProgram);
+        glDeleteShader(vertexShader);
+        glDeleteShader(fragmentShader);
+
+        glVertexAttribPointer(0, 3, GL_FLOAT, false, 12, NULL);
+        glEnableVertexAttribArray(0);
+
+
         while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            glUseProgram(shaderProgram);
+            glBindVertexArray(vao);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
             glfwSwapBuffers(window);
 
