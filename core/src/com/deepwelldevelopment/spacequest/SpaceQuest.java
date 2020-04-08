43f0d0560cdbd5be69f2c8f635ea86f10a51fbc9
cjs8487 <cjschaefer1999@gmail.com>
2020-04-08 16:02:04 -0400

    Basic rendering

--- core/src/com/deepwelldevelopment/spacequest/SpaceQuest.java
+++ core/src/com/deepwelldevelopment/spacequest/SpaceQuest.java
@@ -1,12 +1,50 @@
 package com.deepwelldevelopment.spacequest;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
+import com.badlogic.gdx.graphics.VertexAttributes.Usage;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
 
 public class SpaceQuest implements ApplicationListener {
 
+    private PerspectiveCamera camera;
+    private Model model;
+    private ModelInstance modelInstance;
+    private ModelBatch batch;
+    private Environment environment;
+
     @Override
     public void create() {
+        camera = new PerspectiveCamera(67, Gdx.graphics.getWidth(),
+                Gdx.graphics.getHeight()
+        );
+        camera.position.set(10f, 10f, 10f);
+        camera.lookAt(0, 0, 0);
+        camera.near = 1f;
+        camera.far = 300f;
+        camera.update();
 
+        ModelBuilder modelBuilder = new ModelBuilder();
+        model = modelBuilder.createBox(5f, 5f, 5f,
+                new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+                Usage.Position | Usage.Normal
+        );
+        modelInstance = new ModelInstance(model);
+
+        batch = new ModelBatch();
+
+        environment = new Environment();
+        environment.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f,
+                0.4f, 0.4f, 1f
+        ));
+        environment.add(new DirectionalLight().set(0.8f, 0.8f, 0.8f, -1f,
+                -0.8f, -0.2f));
     }
 
     @Override
@@ -16,7 +54,14 @@ public class SpaceQuest implements ApplicationListener {
 
     @Override
     public void render() {
+        Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(),
+                Gdx.graphics.getHeight()
+        );
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+        batch.begin(camera);
+        batch.render(modelInstance, environment);
+        batch.end();
     }
 
     @Override
@@ -31,6 +76,7 @@ public class SpaceQuest implements ApplicationListener {
 
     @Override
     public void dispose() {
-
+        model.dispose();
+        batch.dispose();
     }
 }
