3b41653753ee4f5a504137dc2846a5ff89b9f22a
cjs8487 <cjschaefer1999@gmail.com>
2020-04-11 00:54:21 -0400

    Began rebuilding the Gui/inventory system

---
+++ core/src/com/deepwelldevelopment/spacequest/client/gui/Gui.java
@@ -0,0 +1,30 @@
+package com.deepwelldevelopment.spacequest.client.gui;
+
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.deepwelldevelopment.spacequest.client.gui.component.GuiComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Gui {
+
+    public static final float GUI_SCALE = 3.0f;
+
+    private List<GuiComponent> components;
+
+    public Gui() {
+        this.components = new ArrayList<>();
+    }
+
+    public void render(Batch batch, int mouseX, int mouseY) {
+        for (GuiComponent component : components) {
+            component.render(batch, mouseX, mouseY);
+        }
+    }
+
+    public void mouseClicked(int x, int y, int button) {
+    }
+
+    public void keyTyped(int code) {
+    }
+}
